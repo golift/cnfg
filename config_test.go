961061d377655468e9da4a9333e71b9b77402470
davidnewhall2 <david@sleepers.pro>
2019-12-18 23:40:55 -0800

    fix name for godoc2

--- config_test.go
+++ config_test.go
@@ -45,7 +45,7 @@ func (t *TimeX) UnmarshalENV(tag, val string) error {
 
 // This simple example shows how you may use the ENVUnmarshaler interface.
 // This shows how to use two environment variables to set one custom value.
-func ExampleENVUnmarshaler_twoEnvVariables() {
+func ExampleENVUnmarshaler() {
 	c := &Config{}
 
 	os.Setenv("APP_IN", "5m")
